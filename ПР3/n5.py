08d6bc02c30f000e9fa5a5c9e5a169562a3b4db0
Bobishkins <timum58@gmail.com>
2026-09-25 11:56:54 +0300

    Create n5.py

---
+++ ПР3/n5.py
@@ -0,0 +1,5 @@
+shopping_list = ["Молоко", "Сок", "Банан" ]
+shopping_list.append("Яблоко")
+print(shopping_list)
+unique_items = (set(shopping_list))
+print(unique_items) # Множество не сохраняет порядок и убирает дубликаты, поэтому порядок вывода отличается от списка.
